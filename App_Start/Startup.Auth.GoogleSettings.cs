0c2a178c53bc8647a6f0da81c4d4effc8d40e3ef
Anders Abel <anders@abel.nu>
2014-11-06 15:03:05 +0100

    Added middleware setup for Google.

---
+++ App_Start/Startup.Auth.GoogleSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialLoginWithoutIdentity
+{
+    public partial class Startup
+    {
+#error Register with Google and set your client id and client secret here.
+        private const string GoogleClientId = "";
+        private const string GoogleClientSecret = "";
+    }
+}
---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Google;
+
+namespace SocialLoginWithoutIdentity
+{
+	public partial class Startup
+	{
+		private void ConfigureAuth(IAppBuilder app)
+        {
+			const string authenticationType = "ApplicationCookie";
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+					AuthenticationType = authenticationType,
+					LoginPath = new PathString("/Account/Login")
+                });
+
+			app.SetDefaultSignInAsAuthenticationType(authenticationType);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+                {
+                    ClientId = GoogleClientId,
+                    ClientSecret = GoogleClientSecret
+                });
+        }
+	}
+}

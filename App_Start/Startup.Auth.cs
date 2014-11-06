d10c6942e403a0c84dfca6daee1ed4c9db81dcfa
Anders Abel <anders@abel.nu>
2014-11-06 23:14:41 +0100

    Indentation of Startup.Auth.cs

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -10,19 +10,19 @@ using Microsoft.Owin.Security.Google;
 
 namespace SocialLoginWithoutIdentity
 {
-	public partial class Startup
-	{
-		private void ConfigureAuth(IAppBuilder app)
+    public partial class Startup
+    {
+        private void ConfigureAuth(IAppBuilder app)
         {
-			const string authenticationType = "ApplicationCookie";
+            const string authenticationType = "ApplicationCookie";
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
                 {
-					AuthenticationType = authenticationType,
-					LoginPath = new PathString("/Account/Login")
+                    AuthenticationType = authenticationType,
+                    LoginPath = new PathString("/Account/Login")
                 });
 
-			app.SetDefaultSignInAsAuthenticationType(authenticationType);
+            app.SetDefaultSignInAsAuthenticationType(authenticationType);
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
                 {
@@ -30,5 +30,5 @@ namespace SocialLoginWithoutIdentity
                     ClientSecret = GoogleClientSecret
                 });
         }
-	}
+    }
 }

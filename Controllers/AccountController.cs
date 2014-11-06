4bc3f44194134f1dd8c971b0779d8ac7cc5fe137
Anders Abel <anders@abel.nu>
2014-11-06 23:15:04 +0100

    Added AccountController to handle external login.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialLoginWithoutIdentity.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            // Request a redirect to the external login provider
+            return new ChallengeResult("Google",
+                Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
+        }
+
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            return new RedirectResult(returnUrl);
+        }
+
+        // Implementation copied from a standard MVC Project, with some stuff
+        // that relates to linking a new external login to an existing identity
+        // account commented out.
+        private class ChallengeResult : HttpUnauthorizedResult
+        {
+            public ChallengeResult(string provider, string redirectUri)
+            //    : this(provider, redirectUri, null)
+            {
+            //}
+
+            //public ChallengeResult(string provider, string redirectUri, string userId)
+            //{
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+            //    UserId = userId;
+            }
+
+            public string LoginProvider { get; set; }
+            public string RedirectUri { get; set; }
+            //public string UserId { get; set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties() { RedirectUri = RedirectUri };
+            //    if (UserId != null)
+            //    {
+            //        properties.Dictionary[XsrfKey] = UserId;
+            //    }
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+            }
+        }
+    }
+}

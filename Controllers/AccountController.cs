33cd13e1c58007b73e50584eccbfe03e85e042f5
Anders Abel <anders@abel.nu>
2014-11-07 09:46:07 +0100

    Removed commented out stuff in ChallengResult class.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -24,32 +24,21 @@ namespace SocialLoginWithoutIdentity.Controllers
 
         // Implementation copied from a standard MVC Project, with some stuff
         // that relates to linking a new external login to an existing identity
-        // account commented out.
+        // account removed.
         private class ChallengeResult : HttpUnauthorizedResult
         {
             public ChallengeResult(string provider, string redirectUri)
-            //    : this(provider, redirectUri, null)
             {
-            //}
-
-            //public ChallengeResult(string provider, string redirectUri, string userId)
-            //{
                 LoginProvider = provider;
                 RedirectUri = redirectUri;
-            //    UserId = userId;
             }
 
             public string LoginProvider { get; set; }
             public string RedirectUri { get; set; }
-            //public string UserId { get; set; }
 
             public override void ExecuteResult(ControllerContext context)
             {
                 var properties = new AuthenticationProperties() { RedirectUri = RedirectUri };
-            //    if (UserId != null)
-            //    {
-            //        properties.Dictionary[XsrfKey] = UserId;
-            //    }
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
